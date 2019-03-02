6be1a6e56ea0c086860510d8cb74be0c897cff67
Ablam <apple@korays-macbook-air.local>
2019-03-02 20:39:37 +0100

    Initial Commit

---
+++ The Coffee Advisor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Coffee Advisor
+//
+//  Created by Koray Karakurt on 2.03.2019.
+//  Copyright © 2019 Koray Karakurt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
