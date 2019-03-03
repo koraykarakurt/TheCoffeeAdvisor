eca54d14e7e8d02af868aeab84b043b256853c50
Ablam <apple@korays-macbook-air.local>
2019-03-03 14:50:06 +0100

    option array is added. optionPicker.selectRow is implemented.

--- The Coffee Advisor/ViewController.swift
+++ The Coffee Advisor/ViewController.swift
@@ -12,9 +12,48 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+ 
+        
     }
-
-
+    
+    
 }
 
+class ViewController2: UIViewController, UIPickerViewDelegate,UIPickerViewDataSource {
+    
+    
+    
+    let optionArray = ["Blonde Roast", "Medium Roast", "Dark Roast", "Milky", "Sugar-Free", "Kcal < 50", "Kcal < 100", "Kcal < 200"]
+    
+
+    //Pre-setup IBOutlets
+    @IBOutlet weak var optionPicker: UIPickerView!
+
+    //FUNCTIONS
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return optionArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return optionArray[row]
+    }
+ 
+
+    
+    //////////////////////////////
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        optionPicker.delegate = self
+        optionPicker.dataSource = self
+        optionPicker.selectRow(optionArray.count/2, inComponent: 0, animated: false)
+        
+    }
+    
+    
+}
