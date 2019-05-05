8dd3fe2eed9992c2aba3c7af3c5e11d1c2644dcd
Meric Koray <mkoraykarakurt@hotmail.com>
2019-05-05 17:25:58 +0200

    05.05.2019 commit 3
    menu class structure changed.

--- The Coffee Advisor/ViewController.swift
+++ The Coffee Advisor/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 
-
 import UIKit
+import UIMultiPicker
+
 
 class ViewController: UIViewController {
     
@@ -23,13 +24,6 @@ class ViewController: UIViewController {
 }
 
 
-
-    
-
-
-import UIKit
-import UIMultiPicker
-
 class ViewController2: UIViewController {
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -39,14 +33,12 @@ class ViewController2: UIViewController {
 
     static let TASTES = [
 
-        "No Milk"               ,
-        "No Sugar"              ,
+        "Milk-free"             ,//no milk
+        "Glucose-free"          ,//no sugar
         //"No Caffeine"         ,
-        
         //"Blonde Roast"        ,
         //"Medium Roast"        ,
         //"Dark Roast"          ,
-
         "Foam"                  ,
         "Cream"                 ,
         "Extra Shot"            ,
@@ -101,41 +93,75 @@ class ViewController3: UIViewController, UIPickerViewDelegate,UIPickerViewDataSo
     
     var V3_saved_selectedIndexes = UserDefaults.standard.object(forKey:"UIMultiPickerIndexes") as? [Int] ?? [Int]()
     
-
-    
-    
-    
     //let optionArray = ["Blonde Roast", "Medium Roast", "Dark Roast", "Milky", "Sugar-Free", "Kcal < 50", "Kcal < 100", "Kcal < 200"]
     
-   class MyCoffeeMenu {
-
-        var coffeeMenuNames  = [""]
-        var coffeeMenuKcals  = [""]
-
-//        init(coffeeMenuNames: String, coffeeMenuKcals: String) {
-//            self.coffeeMenuNames = ["a"]
-//            self.coffeeMenuKcals = ["77"]
-//
-//        }
-
+    class MyCoffee {
+        var coffeeName      = ""
+        var coffeeKcal      = ""
+        var hasMilk         = false
+        var hasSugar        = false
+        var hasFoam         = false
+        var hasCream        = false
+        var KcalLessThan50  = false
+        var KcalLessThan100 = false
+        var KcalLessThan200 = false
+        var hasExtraShot    = false
+        var isIced          = false
+    
+        init(coffeeName: String, coffeeKcal: String, hasMilk: Bool, hasSugar: Bool, hasFoam: Bool, hasCream: Bool, KcalLessThan50: Bool, KcalLessThan100: Bool, KcalLessThan200: Bool, hasExtraShot: Bool, isIced: Bool) {
+        
+            self.coffeeName         = coffeeName
+            self.coffeeKcal         = coffeeKcal
+            self.hasMilk            = hasMilk
+            self.hasSugar           = hasSugar
+            self.hasFoam            = hasFoam
+            self.hasCream           = hasCream
+            self.KcalLessThan50     = KcalLessThan50
+            self.KcalLessThan100    = KcalLessThan100
+            self.KcalLessThan200    = KcalLessThan200
+            self.hasExtraShot       = hasExtraShot
+            self.isIced             = isIced
+            
+            
+            
+        }
     }
 
-    var myVariableMenu = MyCoffeeMenu()
+    let espresso = MyCoffee(coffeeName: "Espresso", coffeeKcal: "7 Kcal", hasMilk: false, hasSugar: false, hasFoam: false, hasCream: false , KcalLessThan50: true , KcalLessThan100: true , KcalLessThan200: true, hasExtraShot: false, isIced: false)
     
+    let macchiato = MyCoffee(coffeeName: "Macchiato", coffeeKcal: "120 Kcal", hasMilk: true, hasSugar: false, hasFoam: true, hasCream: false , KcalLessThan50: false , KcalLessThan100: true, KcalLessThan200: true, hasExtraShot: false, isIced: false)
     
+    let latte = MyCoffee(coffeeName: "Latte", coffeeKcal: "210 Kcal", hasMilk: true, hasSugar: false, hasFoam: true, hasCream: false, KcalLessThan50: false, KcalLessThan100: false, KcalLessThan200: false, hasExtraShot: false, isIced: false)
     
+    let icedLatte = MyCoffee(coffeeName: "Latte", coffeeKcal: "210 Kcal", hasMilk: true, hasSugar: false, hasFoam: true, hasCream: false, KcalLessThan50: false, KcalLessThan100: false, KcalLessThan200: false, hasExtraShot: false, isIced: true)
     
-  
+    let cappucino = MyCoffee(coffeeName: "Cappucino", coffeeKcal: "150 Kcal", hasMilk: true, hasSugar: false, hasFoam: true, hasCream: false, KcalLessThan50: false, KcalLessThan100: false, KcalLessThan200: true, hasExtraShot: false, isIced: false)
+    
+    let americano = MyCoffee(coffeeName: "Americano", coffeeKcal: "8 Kcal", hasMilk: false, hasSugar: false, hasFoam: false, hasCream: false, KcalLessThan50: true, KcalLessThan100: true, KcalLessThan200: true, hasExtraShot: false, isIced: false)
+    
+    let icedAmericano = MyCoffee(coffeeName: "Iced Americano", coffeeKcal: "8 Kcal", hasMilk: false, hasSugar: false, hasFoam: false, hasCream: false, KcalLessThan50: true, KcalLessThan100: true, KcalLessThan200: true, hasExtraShot: false, isIced: true)
+    
+    let misto = MyCoffee(coffeeName: "Misto", coffeeKcal: "70 Kcal", hasMilk: true, hasSugar: false, hasFoam: false, hasCream: false, KcalLessThan50: false, KcalLessThan100: true, KcalLessThan200: true, hasExtraShot: false, isIced: false)
+    
+    let flatWhite = MyCoffee(coffeeName: "Flat White", coffeeKcal: "190 Kcal", hasMilk: true, hasSugar: false, hasFoam: false, hasCream: false, KcalLessThan50: false, KcalLessThan100: true, KcalLessThan200: true, hasExtraShot: false, isIced: false)
+    
+    let mocha = MyCoffee(coffeeName: "Mocha", coffeeKcal: "190 Kcal", hasMilk: true, hasSugar: false, hasFoam: false, hasCream: true, KcalLessThan50: false, KcalLessThan100: true, KcalLessThan200: true, hasExtraShot: false, isIced: false)
+    
+//    espresso
+//    macchiato
+//    latte
+//    icedLatte
+//    cappucino
+//    americano
+//    misto
+//    flatWhite
+//    mocha
+    
 
-    //coffeeMenu.coffeeMenuNames = ["Cortado"]
-
-    //var coffeeMenu = [""]
+    
+    
     
     var coffeeMenu = [""]
-    
-
-   
-    
     
     //Pre-setup IBOutlets
     
@@ -150,28 +176,28 @@ class ViewController3: UIViewController, UIPickerViewDelegate,UIPickerViewDataSo
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        
+
     coffeeMenu =
         [
         "Lungo"                        ,
         "Espresso"                     ,
-        "Macchiato"                    ,
         "Ristretto"                    ,
         "Americano"                    ,
-            
+
         "Latte"                        ,//milk
+        "Macchiato"                    ,//milk
         "Misto"                        ,//milk
         "Cappucino"                    ,//milk
         "Flat White"                   ,//milk
         "Cortado"                      ,//milk
 
         //"Ristretto Bianco"           ,
-        
+
         "Mocha"                        ,//milk+sugar
         "Frappuccino"                  ,//milk+sugar
         "Caramel Macchiato"            ,//milk+sugar
         "White Chocolate Mocha"        ,//milk+sugar
-        
+
         "Iced Latte"                   ,//milk
         "Iced Mocha"                   ,//milk+sugar
         "Iced Americano"               ,
@@ -179,30 +205,46 @@ class ViewController3: UIViewController, UIPickerViewDelegate,UIPickerViewDataSo
         "Iced White Chocolate Mocha"    //milk+sugar
         ]
         
-        if V3_saved_selectedIndexes.contains(0)  { //no milk and sugar case
+        if V3_saved_selectedIndexes.contains(0)  { //no milk
+          
             coffeeMenu.removeAll(keepingCapacity: false)
-            coffeeMenu.append("Lungo")
-            coffeeMenu.append("Espresso")
-            coffeeMenu.append("Ristretto")
-            coffeeMenu.append("Americano")
-            coffeeMenu.append("Iced Americano")
+            
+            coffeeMenu.append(americano.coffeeName+", "+americano.coffeeKcal)
+            coffeeMenu.append(espresso.coffeeName+", "+espresso.coffeeKcal)
+            coffeeMenu.append(icedAmericano.coffeeName+", "+icedAmericano.coffeeKcal)
+//            coffeeMenu.removeAll(keepingCapacity: false)
+//            coffeeMenu.append("Lungo")
+//            coffeeMenu.append("Espresso")
+//            coffeeMenu.append("Ristretto")
+//            coffeeMenu.append("Americano")
+//            coffeeMenu.append("Iced Americano")
         }
         else if V3_saved_selectedIndexes.contains(1)  { //no sugar case
             coffeeMenu.removeAll(keepingCapacity: false)
-            coffeeMenu.append("Lungo")
-            coffeeMenu.append("Espresso")
-            coffeeMenu.append("Macchiato")
-            coffeeMenu.append("Ristretto")
-            coffeeMenu.append("Americano")
+
+            coffeeMenu.append(americano.coffeeName)
+            coffeeMenu.append(espresso.coffeeName)
+            coffeeMenu.append(flatWhite.coffeeName)
+            coffeeMenu.append(latte.coffeeName)
+            coffeeMenu.append(misto.coffeeName)
             
-            coffeeMenu.append("Latte")
-            coffeeMenu.append("Misto")
-            coffeeMenu.append("Cappucino")
-            coffeeMenu.append("Flat White")
-            coffeeMenu.append("Cortado")
+            coffeeMenu.append(icedAmericano.coffeeName)
+            coffeeMenu.append(icedLatte.coffeeName)
             
-            coffeeMenu.append("Iced Americano")
-            coffeeMenu.append("Iced Latte")
+//            coffeeMenu.append("Lungo")
+//            coffeeMenu.append("Espresso")
+//            coffeeMenu.append("Macchiato")
+//            coffeeMenu.append("Ristretto")
+//            coffeeMenu.append("Americano")
+//
+//            coffeeMenu.append("Latte")
+//            coffeeMenu.append("Misto")
+//            coffeeMenu.append("Cappucino")
+//            coffeeMenu.append("Flat White")
+//            coffeeMenu.append("Cortado")
+//
+//            coffeeMenu.append("Iced Americano")
+//            coffeeMenu.append("Iced Latte")
             
         }
         
@@ -257,11 +299,9 @@ class ViewController3: UIViewController, UIPickerViewDelegate,UIPickerViewDataSo
         pickerLabel.backgroundColor = UIColor.white
         pickerLabel.textAlignment = .center
         return pickerLabel
-        
-        
+      
     }
-    
-    
+
     //////////////////////////////
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -339,3 +379,5 @@ class ViewController3: UIViewController, UIPickerViewDelegate,UIPickerViewDataSo
 //
 //
 //
+
+
